2babd5d2e6dfe96f34f846df907fcac158838a0b
pers1307 <hunterofwallstreet@mail.ru>
2024-02-13 18:49:53 +0500

    Дашборд. Вывел состояние всех счетов

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\DashboardService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard_index')]
-    public function index(): Response
+    public function index(DashboardService $dashboardService): Response
     {
-        return $this->render('dashboard/index.html.twig', []);
+        if ($this->getParameter('app.isDebug')) {
+            $accauntsDataForGraph = $dashboardService->getAccauntsDataForGraph();
+        } else {
+            $accauntsDataForGraph = $dashboardService->getCachedAccauntsDataForGraph();
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'accauntsDataForGraph' => $accauntsDataForGraph,
+        ]);
     }
 }
---
+++ src/Service/DashboardService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Accaunt;
+use App\Entity\AccauntHistory;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class DashboardService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private CacheInterface $cache,
+    ) {
+    }
+
+    public function getCachedAccauntsDataForGraph(): array
+    {
+        return $this->cache->get('dashboard_accaunts', function (CacheItemInterface $cacheItem) {
+            $cacheItem->expiresAfter(60 * 60 * 24);
+            return $this->getAccauntsDataForGraph();
+        });
+    }
+
+    public function getAccauntsDataForGraph(): array
+    {
+        $accauntRepository = $this->entityManager->getRepository(Accaunt::class);
+        $accaunts = $accauntRepository->findAll();
+
+        $result = [];
+        foreach ($accaunts as $accaunt) {
+            $result[$accaunt->getTitle()] = $this->getFormatGraphDataByAccaunt($accaunt);
+        }
+
+        return $result;
+    }
+
+    private function getFormatGraphDataByAccaunt(Accaunt $accaunt): string
+    {
+        $accauntHistoryRepository = $this->entityManager->getRepository(AccauntHistory::class);
+        $accauntHistoryItems = $accauntHistoryRepository->findBy(['accaunt' => $accaunt->getId()], ['created' => 'ASC']);
+
+        return $this->formatGraphData($accauntHistoryItems);
+    }
+
+    private function formatGraphData(array $accauntHistoryItems): string
+    {
+        return json_encode(
+            [
+                'labels' => array_map(static fn(AccauntHistory $accauntHistory) => $accauntHistory->getCreated()->format('Y-m-d'), $accauntHistoryItems),
+                'values' => array_map(
+                    static fn(AccauntHistory $accauntHistory) => intval($accauntHistory->getValue()),
+                    $accauntHistoryItems
+                ),
+            ]
+        );
+    }
+}
