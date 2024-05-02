e244bf3ed943a7612dc509242be6ac4e0b989278
pers1307 <hunterofwallstreet@mail.ru>
2024-05-02 10:03:47 +0500

    Справочники. Акции. Логировать не корректное обновление

--- src/Command/SyncDictionariesCommand.php
+++ src/Command/SyncDictionariesCommand.php
@@ -3,15 +3,14 @@
 namespace App\Command;
 
 use App\Service\StockService;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 #[AsCommand(
     name: 'app:sync-dictionaries',
@@ -20,21 +19,26 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class SyncDictionariesCommand extends Command
 {
     public function __construct(
-        private readonly StockService $stockService
+        private readonly StockService $stockService,
+        private readonly LoggerInterface $dictionaryLogger,
     ) {
         parent::__construct();
     }
 
     /**
-     * @throws TransportExceptionInterface
      * @throws ServerExceptionInterface
      * @throws RedirectionExceptionInterface
-     * @throws DecodingExceptionInterface
      * @throws ClientExceptionInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->stockService->update();
+        try {
+            $this->stockService->update();
+        } catch (\Throwable $exception) {
+            $this->dictionaryLogger->error($exception);
+
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
