f3b41893b0ba53ff5e9fde000a0e6287934cf76d
Перескоков Юрий <y.pereskokov@dvizh.io>
2024-08-14 20:43:45 +0500

    Дашборд. Убрал кеширование

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,18 +10,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     public function __construct(
-        private bool $isDebug
+//        private bool $isDebug
     ) {
     }
 
     #[Route('/', name: 'app_dashboard_index')]
     public function index(DashboardService $dashboardService): Response
     {
-        if ($this->isDebug) {
-            $accauntsDataForGraph = $dashboardService->getAccauntsDataForGraph();
-        } else {
-            $accauntsDataForGraph = $dashboardService->getCachedAccauntsDataForGraph();
-        }
+        $accauntsDataForGraph = $dashboardService->getAccauntsDataForGraph();
+
+//        if ($this->isDebug) {
+//            $accauntsDataForGraph = $dashboardService->getAccauntsDataForGraph();
+//        } else {
+//            $accauntsDataForGraph = $dashboardService->getCachedAccauntsDataForGraph();
+//        }
 
         return $this->render('dashboard/index.html.twig', [
             'accauntsDataForGraph' => $accauntsDataForGraph,
