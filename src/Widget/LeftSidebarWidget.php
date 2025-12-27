745d1ffca9fc93cc5070c84017ec6caee8581a2d
Перескоков Юрий <y.pereskokov@dvizh.io>
2025-12-27 12:20:05 +0500

    Меню. Сделал активными разделы

--- src/Widget/LeftSidebarWidget.php
+++ src/Widget/LeftSidebarWidget.php
@@ -3,21 +3,23 @@
 namespace App\Widget;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 class LeftSidebarWidget extends AbstractController
 {
     public function __construct(
-        //        private readonly AccauntService $accauntService
-    )
-    {
+        private readonly RequestStack $requestStack
+    ) {
     }
 
     public function show(): Response
     {
-        //        $accaunts = $this->accauntService->findAll();
+        $mainRequest = $this->requestStack->getMainRequest();
+        $currentRoute = $mainRequest?->attributes->get('_route');
+
         return $this->render('widgets/left-sidebar/show.html.twig', [
-            //            'accaunts' => $accaunts,
+            'currentRoute' => $currentRoute,
         ]);
     }
 }
